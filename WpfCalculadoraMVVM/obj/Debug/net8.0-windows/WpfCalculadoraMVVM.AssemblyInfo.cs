720a221b09a658f2d3c344424c5756f45a330955
Milciades Gonzalez <mega.tecnica.1000@gmail.com>
2025-06-10 19:56:42 -0400

    se agrego comentarios en el archivo

--- WpfCalculadoraMVVM/obj/Debug/net8.0-windows/WpfCalculadoraMVVM.AssemblyInfo.cs
+++ WpfCalculadoraMVVM/obj/Debug/net8.0-windows/WpfCalculadoraMVVM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfCalculadoraMVVM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0fb498d8a97abf00922ea0974c633a1809b35d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfCalculadoraMVVM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfCalculadoraMVVM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
